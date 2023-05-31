445a200c2e259d31b147d20033aec9ac6ae0275c
Yevhen Danilov <danilov.evgen@gmail.com>
2023-05-31 18:03:27 +0300

    css to sass

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,83 @@
-$('.clients-slider').slick({
-  infinite: true,
-  slidesToShow: 6,
-  slidesToScroll: 1,
-  arrows: true,
-  dots: false,
-});
+// $('.clients-slider').slick({
+//   infinite: true,
+//   slidesToShow: 6,
+//   slidesToScroll: 1,
+//   arrows: true,
+//   dots: false,
+// });
+
+function myFunction(tabletSize, mobileSize) {
+  // switch (size) {
+  //   case desctopSize.matches:
+  //     document.body.style.backgroundColor = 'red';
+  //     $('.clients-slider').slick({
+  //       infinite: true,
+  //       slidesToShow: 6,
+  //       slidesToScroll: 1,
+  //       arrows: true,
+  //       dots: false,
+  //     });
+  //     break;
+  //   case tabletSize.matches:
+  //     document.body.style.backgroundColor = 'pink';
+  //     $('.clients-slider').slick({
+  //       infinite: true,
+  //       slidesToShow: 3,
+  //       slidesToScroll: 1,
+  //       arrows: false,
+  //       dots: false,
+  //     });
+  //     break;
+
+  //   default:
+  //     document.body.style.backgroundColor = 'yellow';
+  //     $('.clients-slider').slick({
+  //       infinite: true,
+  //       slidesToShow: 1,
+  //       slidesToScroll: 1,
+  //       arrows: false,
+  //       dots: false,
+  //     });
+  //     break;
+  // }
+
+  if (mobileSize.matches) {
+    // If media query matches
+    document.body.style.backgroundColor = 'yellow';
+    $('.clients-slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: false,
+    });
+  } else if (tabletSize.matches) {
+    document.body.style.backgroundColor = 'pink';
+    $('.clients-slider').slick({
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      arrows: false,
+      dots: false,
+    });
+  } else {
+    document.body.style.backgroundColor = 'red';
+    $('.clients-slider').slick({
+      infinite: true,
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      arrows: true,
+      dots: false,
+    });
+  }
+}
+
+var mobileSize = window.matchMedia('(max-width: 480px)');
+var tabletSize =
+  window.matchMedia('(min-width: 481px)') && window.matchMedia('(max-width: 1200px)');
+var desctopSize = window.matchMedia('(min-width: 1201px)');
+
+myFunction(tabletSize, mobileSize); // Call listener function at run time
+mobileSize.addListener(myFunction); // Attach listener function on state changes
+tabletSize.addListener(myFunction); // Attach listener function on state changes
+desctopSize.addListener(myFunction); // Attach listener function on state changes
