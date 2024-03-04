c0b0512aa19a8f3f50e44b1c5492940413c86c93
Yevhen Danilov <danilov.evgen@gmail.com>
2024-03-04 16:20:05 +0200

    add content in flagpoles

--- js/convertPrices.js
+++ js/convertPrices.js
@@ -2,35 +2,39 @@ getPrices();
 convertPrices();
 
 function getPrices() {
-  const prices = document.querySelectorAll('.price');
-  const pricesArr = [];
+  if (document.title !== 'Корзина') {
+    const prices = document.querySelectorAll('.price');
+    const pricesArr = [];
 
-  for (let i = 0; i < prices.length; i++) {
-    pricesArr[i] = prices[i].innerHTML.replace(/[^\d\.]*/g, '');
+    for (let i = 0; i < prices.length; i++) {
+      pricesArr[i] = prices[i].innerHTML.replace(/[^\d\.]*/g, '');
+    }
+    window.localStorage.setItem('pricesArr', JSON.stringify(pricesArr));
   }
-  window.localStorage.setItem('pricesArr', JSON.stringify(pricesArr));
 }
 
 function convertPrices(currency = 'UAH', elem) {
-  if (elem) {
-    elem.parentElement.querySelector('.current').classList.remove('current');
-    elem.classList.add('current');
-  }
+  if (document.title !== 'Корзина') {
+    if (elem) {
+      elem.parentElement.querySelector('.current').classList.remove('current');
+      elem.classList.add('current');
+    }
 
-  const prices = document.querySelectorAll('.price');
-  const pricesArr = JSON.parse(window.localStorage.getItem('pricesArr'));
+    const prices = document.querySelectorAll('.price');
+    const pricesArr = JSON.parse(window.localStorage.getItem('pricesArr'));
 
-  for (let i = 0; i < prices.length; i++) {
-    const eurCourse = 42 / 42;
-    const uahCourse = 42 * eurCourse;
-    const usdCourse = (42 / 38) * eurCourse;
+    for (let i = 0; i < prices.length; i++) {
+      const eurCourse = 42 / 42;
+      const uahCourse = 42 * eurCourse;
+      const usdCourse = (42 / 38) * eurCourse;
 
-    if (currency === 'UAH') {
-      prices[i].innerHTML = pricesArr[i] * uahCourse + ' ₴';
-    } else if (currency === 'USD') {
-      prices[i].innerHTML = Math.round(pricesArr[i] * usdCourse * 100) / 100 + ' $';
-    } else {
-      prices[i].innerHTML = Math.round(pricesArr[i] * eurCourse * 100) / 100 + ' €';
+      if (currency === 'UAH') {
+        prices[i].innerHTML = pricesArr[i] * uahCourse + ' ₴';
+      } else if (currency === 'USD') {
+        prices[i].innerHTML = Math.round(pricesArr[i] * usdCourse * 100) / 100 + ' $';
+      } else {
+        prices[i].innerHTML = Math.round(pricesArr[i] * eurCourse * 100) / 100 + ' €';
+      }
     }
   }
 }
